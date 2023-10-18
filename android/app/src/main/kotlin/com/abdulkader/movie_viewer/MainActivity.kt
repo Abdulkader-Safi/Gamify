ddcc35627e1c36a142d43202dc8481198b756b1e
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-18 23:52:56 +0300

    Gamify

---
+++ android/app/src/main/kotlin/com/abdulkader/movie_viewer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.abdulkader.movie_viewer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:movie_viewer/data.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _deviceHeight;
+  var _deviceWidth;
+
+  var _selectedGame;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedGame = 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: [
+          _featuredGameWidget(),
+          _gradientBoxWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _featuredGameWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.5,
+      width: _deviceWidth,
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        children: featuredGames.map((game) {
+          return Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(game.coverImage.url),
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _gradientBoxWidget() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight * 0.80,
+        width: _deviceWidth,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(35, 45, 59, 1.0),
+              Colors.transparent,
+            ],
+            stops: [0.65, 1.0],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          ),
+        ),
+      ),
+    );
+  }
+}
