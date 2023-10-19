d30e53d03863ae5783734bd4623764621dd4e871
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-10-19 13:22:11 +0300

    _topBarWidget()

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -29,6 +29,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           _featuredGameWidget(),
           _gradientBoxWidget(),
+          _topLayoutWidget(),
         ],
       ),
     );
@@ -72,6 +73,58 @@ class _HomePageState extends State<HomePage> {
             end: Alignment.topCenter,
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _topLayoutWidget() {
+    return Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: _deviceWidth * 0.05,
+        vertical: _deviceHeight * 0.005,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          _topBarWidget(),
+        ],
+      ),
+    );
+  }
+
+  Widget _topBarWidget() {
+    return SizedBox(
+      height: _deviceHeight * 0.13,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Icon(
+            Icons.menu,
+            color: Colors.white,
+            size: 30,
+          ),
+          Row(
+            children: [
+              const Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 30,
+              ),
+              SizedBox(
+                width: _deviceWidth * 0.03,
+              ),
+              const Icon(
+                Icons.notifications_none,
+                color: Colors.white,
+                size: 30,
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
